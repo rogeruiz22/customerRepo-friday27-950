c4bf3dab90b45c43cc52e8ecb32b53592003d07f
Roger Ruiz <roger.ruiz@oktana.com>
2022-05-30 18:35:17 -0400

    sdsdsdsdsd

--- src/test/java/ClasePruebaTest.java
+++ src/test/java/ClasePruebaTest.java
@@ -21,7 +21,7 @@ public class ClasePruebaTest {
 
         Thread.sleep(8000);
 
-        WebElement selectAllContactsCheckbox = driver.findElement(By.xpath("(//span[@class='slds-checkbox--faux'])[1]"));
+//        WebElement selectAllContactsCheckbox = driver.findElement(By.xpath("(//span[@class='slds-checkbox--faux'])[1]"));
         WebElement selectRogerContact = driver.findElement(By.xpath("(//a[text()='Roger Ruiz']/ancestor::tr//td/following-sibling::td[@class='slds-cell-edit cellContainer'])[1]"));
 //        WebElement selectDavidContact = driver.findElement(By.xpath("(//a[text()='David Heredia']/ancestor::tr//td/following-sibling::td[@class='slds-cell-edit cellContainer'])[1]"));
         selectRogerContact.click();
