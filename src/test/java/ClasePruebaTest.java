384523858e8ea916b775046e794961b57a8e6812
Roger Ruiz <roger.ruiz@oktana.com>
2022-05-30 16:18:12 -0400

    cansado

---
+++ src/test/java/ClasePruebaTest.java
@@ -0,0 +1,9 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ClasePruebaTest {
+    @Test
+    public void primerTest() {
+        Assert.assertTrue(true);
+    }
+}
